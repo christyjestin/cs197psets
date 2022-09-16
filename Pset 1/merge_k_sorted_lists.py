4288be5220914a68589eea5a0a7400a91532d434
Christy Jestin <cjj9000@gmail.com>
2022-09-15 21:12:46 -0400

    Set up testing code and export environment
    Sets up functions to convert between standard lists and linked lists and check function output

---
+++ Pset 1/merge_k_sorted_lists.py
@@ -0,0 +1,50 @@
+from typing import *
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_k_sorted_lists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    return ListNode()
+
+# recursively construct linked list
+def create_linked_list(lst: List[int]) -> ListNode:
+    return ListNode(val = lst[0], next = create_linked_list(lst[1:])) if lst else None
+
+# process list of lists to create list of ListNodes
+def process_lists(lists: List[List[int]]) -> List[ListNode]:
+    return [create_linked_list(lst) for lst in lists]
+
+# convert linked list to standard list using head
+def create_regular_list(linked_list: ListNode) -> List[int]:
+    output = []
+    head = linked_list
+    while head:
+        output.append(head.val)
+        head = head.next
+    return output
+
+def run_tests(tests, expected):
+    output = [create_regular_list(merge_k_sorted_lists(process_lists(test))) for test in tests]
+    if len(output) != len(expected):
+        print(f"Tests of length {len(tests)} are incompatible with expected answers of length {len(expected)}")
+        return
+    print("Accepted" if output == expected else "Rejected")
+    print("Output: ")
+    for response in output:
+        print(response)
+    print("Expected: ")
+    for answer in expected:
+        print(answer)
+
+
+test_cases = [[[1,4,5],[1,3,4],[2,6]],
+              [],
+              [[]]]
+answers = [[1,1,2,3,4,4,5,6],
+           [],
+           []]
+
+run_tests(test_cases, answers)
